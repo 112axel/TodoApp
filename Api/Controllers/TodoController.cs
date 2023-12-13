5388868f7de0a8298ed5c7b05aeea46339f35ae9
112axel <axel.cullbrand@gmail.com>
2023-12-13 15:47:23 +0100

    removed more template code

--- Api/Controllers/TodoController.cs
+++ Api/Controllers/TodoController.cs
@@ -6,11 +6,6 @@ namespace Api.Controllers;
 [Route("[controller]")]
 public class TodoController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
     private readonly ILogger<TodoController> _logger;
 
     public TodoController(ILogger<TodoController> logger)
@@ -18,15 +13,4 @@ public class TodoController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }
 }
