e36cb8f70083c7abe242c32b6084c9245ad3aafd
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-12-14 14:58:35 +0100

    added some tests

--- Tests/TodoServiceTesting.cs
+++ Tests/TodoServiceTesting.cs
@@ -1,3 +1,4 @@
+using Domain.Entities;
 using Services;
 namespace Tests;
 
@@ -20,5 +21,34 @@ public class TodoServiceTesting
 
         Assert.Equal("make food",result[0].Title );
         Assert.Equal("make meatballs",result[0].Description );
+    }
+
+    [Fact]
+    public void CreateNewTodoReturnsSameValue()
+    {
+        //arrange
+        var db = new FakeDataBase();
+        var todoService = new TodoService(db);
+
+        //act
+        var result = todoService.CreateNewTodo(new TodoItem("dishwasher", "empty dishwasher"));
+
+        //assert
+        Assert.Equal("dishwasher", result.Title);
+        Assert.Equal("empty dishwasher", result.Description);
+
+    }
+
+    [Fact]
+    public void CreateNewTodoListIsLongerAfterCreate()
+    {
+        //arrange
+        var db = new FakeDataBase();
+        var todoService = new TodoService(db);
+
+        //act
+        var result = todoService.CreateNewTodo(new TodoItem("dishwasher", "empty dishwasher"));
+
+        Assert.Equal(2, db.TodoItems.Count);
     }
 }
