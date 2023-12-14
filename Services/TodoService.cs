67055c31c4d27c0045c96bb7168cb2590be3865e
Axel Cullbrand <axel.cullbrand@gmail.com>
2023-12-14 14:58:50 +0100

    added code for fufuling tests

--- Services/TodoService.cs
+++ Services/TodoService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace Services
 {
-    internal class TodoService
+    public class TodoService
     {
+        private FakeDataBase dataBase { get; set; }
+        public TodoService(FakeDataBase db)
+        {
+            dataBase = db;
+        }
+
+        public TodoItem CreateNewTodo(TodoItem? item)
+        {
+            if(item is null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            dataBase.TodoItems.Add(item);
+            return item;
+        }
     }
 }
